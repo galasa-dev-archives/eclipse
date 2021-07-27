086c92592446fe7bdf7180ca2ab2d4d24519a52d
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-07-27 14:28:23 +0100

    add additional data to the run view
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

--- galasa-eclipse-parent/dev.galasa.eclipse/src/dev/galasa/eclipse/ui/runs/Run.java
+++ galasa-eclipse-parent/dev.galasa.eclipse/src/dev/galasa/eclipse/ui/runs/Run.java
@@ -1,7 +1,7 @@
 /*
  * Licensed Materials - Property of IBM
  * 
- * (c) Copyright IBM Corp. 2019.
+ * (c) Copyright IBM Corp. 2019-2021.
  */
 package dev.galasa.eclipse.ui.runs;
 
@@ -11,82 +11,145 @@ import dev.galasa.eclipse.ui.PropertyUpdate.Type;
 
 public class Run {
 
-    private final String runName;
+	private final String runName;
 
-    private String       status;
+	private String       status;
+	private String       test;
+	private String       result;
+	private String       requestor;
+	private String       currentTestMethod;
+	private int          totalTestMethods = -1;
+	private int          actualTestMethod = -1;
 
-    private Instant      lastPropertyModification = Instant.now();
+	private Instant      lastPropertyModification = Instant.now();
 
-    private Instant      queued                   = Instant.ofEpochSecond(0);
+	private Instant      queued                   = Instant.ofEpochSecond(0);
 
-    public Run(String runName) {
-        this.runName = runName;
-    }
+	public Run(String runName) {
+		this.runName = runName;
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(runName);
-        sb.append(" - ");
-        if (status != null) {
-            sb.append(status);
-        } else {
-            sb.append("unknown");
-        }
-        return sb.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(runName);
+		sb.append(" - ");
+		if (status != null) {
+			sb.append(status);
+		} else {
+			sb.append("unknown");
+		}
+		sb.append(" - ");
+		sb.append(this.test);
 
-    public synchronized void propertyUpdate(String key, String value, Type type) {
-        if (type == Type.DELETE) {
-            value = null;
-        }
+		if (this.result != null) {
+			sb.append(" - ");
+			sb.append(this.result);
+		}
 
-        switch (key) {
-            case "status":
-                this.status = value;
-                break;
-            case "queued":
-                if (value == null) {
-                    this.queued = Instant.ofEpochSecond(0);
-                } else {
-                    try {
-                        this.queued = Instant.parse(value);
-                    } catch (Throwable e) {
-                        this.queued = Instant.ofEpochSecond(0);
-                    }
-                }
-                break;
-        }
+		if (this.currentTestMethod != null) {
+			sb.append(" - ");
+			sb.append(this.currentTestMethod);
+			sb.append("(");
+			sb.append(Integer.toString(this.actualTestMethod));
+			sb.append(" of ");
+			sb.append(Integer.toString(this.totalTestMethods));
+			sb.append(")");
+		}
+		
+		if (this.requestor != null) {
+			sb.append(" - ");
+			sb.append(this.requestor);
+		}
+		return sb.toString();
+	}
 
-        if (type != Type.DELETE) {
-            this.lastPropertyModification = Instant.now();
-        }
+	public synchronized void propertyUpdate(String key, String value, Type type) {
+		if (type == Type.DELETE) {
+			value = null;
+		}
 
-        return;
-    }
+		switch (key) {
+		case "status":
+			this.status = value;
+			break;
+		case "result":
+			this.result = value;
+			break;
+		case "requestor":
+			this.requestor = value;
+			break;
+		case "queued":
+			if (value == null) {
+				this.queued = Instant.ofEpochSecond(0);
+			} else {
+				try {
+					this.queued = Instant.parse(value);
+				} catch (Throwable e) {
+					this.queued = Instant.ofEpochSecond(0);
+				}
+			}
+			break;
+		case "test":
+			if (value == null) {
+				this.test = "?";
+			} else {
+				String[] testParts = value.split("[/\\.]");
+				if (testParts.length < 1) {
+					this.test = "?";
+				} else {
+					this.test = testParts[testParts.length - 1];
+				}
+			}
+			break;
+		case "method.name":
+			this.currentTestMethod = value;
+			break;
+		case "method.total":
+			try {
+				this.totalTestMethods = Integer.parseInt(value);
+			} catch(Exception e) {
+				this.totalTestMethods = -2;
+			}
+			break;
+		case "method.current":
+			try {
+				this.actualTestMethod = Integer.parseInt(value);
+			} catch(Exception e) {
+				this.actualTestMethod = -2;
+			}
+			break;
+		}
 
-    public synchronized boolean isValid() {
-        if (this.status != null) {
-            return true;
-        }
+		if (type != Type.DELETE) {
+			this.lastPropertyModification = Instant.now();
+		}
 
-        return false;
-    }
+		return;
+	}
 
-    public synchronized boolean updatedRecently() {
-        Instant fiveSecondsAgo = Instant.now().minusSeconds(5);
-        if (lastPropertyModification.compareTo(fiveSecondsAgo) >= 0) {
-            return true;
-        }
-        return false;
-    }
+	public synchronized boolean isValid() {
+		if (this.status != null) {
+			return true;
+		}
 
-    public String getRunName() {
-        return this.runName;
-    }
+		return false;
+	}
 
-    public Instant getQueued() {
-        return this.queued;
-    }
+	public synchronized boolean updatedRecently() {
+		Instant fiveSecondsAgo = Instant.now().minusSeconds(5);
+		if (lastPropertyModification.compareTo(fiveSecondsAgo) >= 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public String getRunName() {
+		return this.runName;
+	}
+
+	public Instant getQueued() {
+		return this.queued;
+	}
 
 }
